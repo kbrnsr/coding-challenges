bef5147f1fa2c2cad92160867bcd88e190bf5154
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-12-01 22:31:41 +0100

    Solved for day 1, part 1

--- aoc-2020/aoc-2020-day01.js
+++ aoc-2020/aoc-2020-day01.js
@@ -1,8 +1,8 @@
-const expenses = [1721, 979, 366, 299, 675, 1456];
+import fs from 'fs';
 
 const returnsTuplesOfTwoNumbersIfSumMatches = (a, sum) => {
   const sumOfTwoNumber = (a, b) => (a + b);
-  sumArray = [];
+  const sumArray = [];
   let i;
   for (i = 0; i < a.length; i++) {
     let j;
@@ -10,6 +10,7 @@ const returnsTuplesOfTwoNumbersIfSumMatches = (a, sum) => {
       const firstNumber = a[i]
       const secondNumber = a[j]
       if (sumOfTwoNumber(firstNumber, secondNumber) === sum) {
+        console.log("pushing");
         sumArray.push([firstNumber, secondNumber]);
       }
     }
@@ -17,4 +18,21 @@ const returnsTuplesOfTwoNumbersIfSumMatches = (a, sum) => {
   return sumArray;
 }
 
-console.log(returnsTuplesOfTwoNumbersIfSumMatches(expenses, 2020));
+const readFileAndReturnArrayOfNumbers = (fileLocation) => {
+  const fileContent = fs.readFileSync(fileLocation, 'utf-8');
+  const stringArray = fileContent.trim().split('\n');
+  const parsedAsIntegersArray = stringArray.map(line => {
+    const trimmedLine = line.trim();
+    if (trimmedLine !== '') {
+      return parseInt(trimmedLine);
+    }
+  });
+  return parsedAsIntegersArray
+}
+
+const solveAocDay01WithInputFile = (fileLocation) => {
+  const integerArray = readFileAndReturnArrayOfNumbers(fileLocation);
+  return returnsTuplesOfTwoNumbersIfSumMatches(integerArray, 2020);
+}
+
+console.log(solveAocDay01WithInputFile('./aoc-2020-day01.input'))
