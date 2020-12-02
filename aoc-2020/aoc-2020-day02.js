74fe99696e7e91a6b30c657ea8f80a00691bf9aa
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-12-02 23:13:57 +0100

    Solved for day 2, part 2

--- aoc-2020/aoc-2020-day02.js
+++ aoc-2020/aoc-2020-day02.js
@@ -34,6 +34,33 @@ const solveDay01part1 = (a) => {
   return validPasswords;
 };
 
-console.log(solveDay01part1(testData))
+const validatePasswordPart2 = (data) => {
+  const re = /(\d+)-(\d+) ([a-z]{1}): ([a-z]*)/;
+  const found = data.match(re);
+  const firstIndex = (parseInt(found[1]) - 1);
+  const secondIndex = (parseInt(found[2]) - 1);
+  const charString = found[3];
+  const rest = found[4];
+  // JS have no logical XOR...
+  const firstBool = (rest[firstIndex] === charString);
+  const secondBool = (rest[secondIndex] === charString);
+  // Gotta love circuit diagrams: (A && NOT B) or (NOT A && B)
+  return ((firstBool && !secondBool) || (!firstBool && secondBool));
+}
+
+const solveDay01part2 = (a) => {
+  let validPasswords = 0;
+  a.map(dataString => {
+    if (validatePasswordPart2(dataString)) {
+      validPasswords += 1;
+    }
+    return null;
+  })
+  return validPasswords;
+};
+
 // const dataArray = readFileAndReturnArrayOfStrings('./aoc-2020-day02.txt')
 // console.log(solveDay01part1(dataArray));
+// console.log(solveDay01part2(dataArray));
+console.log(solveDay01part1(testData))
+console.log(solveDay01part2(testData))
