770f8c643f5469b38916e8d33416b7bc34fa2b1b
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-12-06 17:07:26 +0100

    Almost solved for part 2, some bug in my code gives too low error

--- aoc-2020/aoc-2020-day06.js
+++ aoc-2020/aoc-2020-day06.js
@@ -52,6 +52,33 @@ const solveDay06Part1 = (data) => {
   return result;
 };
 
+const countAllYes = (answers, groupLength) => {
+  const answersValues = Object.values(answers);
+  const groupAllYesCount = answersValues
+    .reduce((allYesCount, answerCount) => {
+      if (answerCount === groupLength) {
+        return allYesCount + 1;
+      } else {
+        return allYesCount;
+      }
+    }, 0)
+  return groupAllYesCount;
+}
+
+const solveDay06Part2 = (data) => {
+  const groupData = transformDataToGroups(data);
+  const answersCount = groupData.map(group => {
+    return countUniqueAnswers(group);
+  })
+  const result = answersCount
+    .reduce((allYes, group) => {
+      const [groupAnswers, groupLength] = group;
+      const groupAllYesCount = countAllYes(groupAnswers, groupLength);
+      return allYes + groupAllYesCount;
+    }, 0)
+  return result;
+};
+
 /* 
 import { readFileAndReturnFileContent }
   from './aoc-2020-api.mjs';
@@ -61,17 +88,23 @@ const data =
 
 console.log("Sum of unique answers in groups"
   , solveDay06Part1(data));
+console.log("Sum of common answers in groups"
+  , solveDay06Part2(data));
  */
 
 /**
  * $ node ./aoc-2020-day06.js
  * Sum of unique answers in groups 6768
+ * Sum of common answers in groups 3484
  */
 
 console.log("Sum of unique answers in groups"
   , solveDay06Part1(testData));
+console.log("Sum of common answers in groups"
+  , solveDay06Part2(testData));
 
 /**
  * $ node ./aoc-2020-day06.js
  * Sum of unique answers in groups 11
+ * Sum of common answers in groups 6
  */
