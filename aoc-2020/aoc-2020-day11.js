0f2f18ac2b05fdf30a180013ed6b3780dc752f06
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-12-12 00:02:52 +0100

    Solved for day 11, part 2

--- aoc-2020/aoc-2020-day11.js
+++ aoc-2020/aoc-2020-day11.js
@@ -11,9 +11,14 @@ const testData = [
   ['L', '.', 'L', 'L', 'L', 'L', 'L', '.', 'L', 'L']
 ]
 
+const FLOOR = '.', EMPTY = 'L', OCCUPIED = '#';
+
 const SeatingSystem = (seats) => {
-  const FLOOR = '.', EMPTY = 'L', OCCUPIED = '#';
   const dimension = seats.length;
+  const steps = [[1, 0], [-1, 0], [0, -1], [0, 1],
+  [-1, -1], [-1, 1], [1, -1], [1, 1]];
+
+  const isValInRange = (val) => (val >= 0 && val < dimension);
 
   const sliceStart = (val) => {
     return (val === 0) ? val : val - 1;
@@ -62,9 +67,88 @@ const SeatingSystem = (seats) => {
     return seat
   };
 
+  const lookInDirection = (stepI, stepJ, seatI, seatJ) => {
+    const nextStepI = seatI + stepI;
+    const nextStepJ = seatJ + stepJ;
+
+    const column = (stepI === 0 && stepJ !== 0);
+    const row = (stepI !== 0 && stepJ === 0);
+    const rowcolumn = (stepI !== 0 && stepJ !== 0);
+
+    if (column) {
+      let j;
+      for (j = nextStepJ; isValInRange(j); j += stepJ) {
+        const seat = seats[nextStepI][j];
+        if (seat === EMPTY || seat === OCCUPIED) {
+          return seat;
+        }
+      }
+    }
+
+    if (row) {
+      let i;
+      for (i = nextStepI; isValInRange(i); i += stepI) {
+        const seat = seats[i][nextStepJ];
+        if (seat === EMPTY || seat === OCCUPIED) {
+          return seat;
+        }
+      }
+    }
+
+    if (rowcolumn) {
+      let i = nextStepI; let j = nextStepJ;
+      while (isValInRange(i) && isValInRange(j)) {
+        const seat = seats[i][j];
+        if (seat === EMPTY || seat === OCCUPIED) {
+          return seat;
+        }
+        i += stepI; j += stepJ;
+      }
+    }
+
+    return FLOOR;
+  };
+
+  const lookAndFindForPos = (seatI, seatJ) => {
+    const whatIsFound = [];
+    let stepIndex;
+    for (stepIndex = 0; stepIndex < steps.length; stepIndex += 1) {
+      const [stepI, stepJ] = steps[stepIndex];
+      const foundChar = lookInDirection(stepI, stepJ, seatI, seatJ);
+      whatIsFound.push(foundChar);
+    }
+    return whatIsFound;
+  };
+
+  const changeSeat2 = (seatI, seatJ) => {
+    const seat = seats[seatI][seatJ];
+    if (seat === FLOOR) { return FLOOR; };
+    const foundSeats = lookAndFindForPos(seatI, seatJ);
+    const [emptyCount, occupiedCount] = foundSeats
+      .reduce(([empties, occupies], curr) => {
+        if (curr === EMPTY) {
+          return [empties + 1, occupies];
+        }
+        if (curr === OCCUPIED) {
+          return [empties, occupies + 1];
+        }
+        return [empties, occupies];
+      }, [0, 0]);
+    if (seat === EMPTY) {
+      if (occupiedCount === 0) { return OCCUPIED; }
+      return seat;
+    }
+    if (seat === OCCUPIED) {
+      if (occupiedCount >= 5) { return EMPTY; }
+      return seat;
+    }
+    return seat
+  };
+
   return {
     changeSeat,
-    countEmptyOccupied
+    countEmptyOccupied,
+    changeSeat2
   };
 };
 
@@ -94,11 +178,44 @@ const playUntilNoChange = (seats) => {
   return lastRound;
 };
 
+const playRound2 = (seats) => {
+  const ss = SeatingSystem(seats);
+  let changed = 0;
+  const newSeats = seats.map((row, i) => {
+    return row.map((val, j) => {
+      const newVal = ss.changeSeat2(i, j);
+      if (val !== newVal) { changed += 1; }
+      return newVal;
+    })
+  });
+  return [changed, newSeats];
+};
+
+const playUntilNoChange2 = (seats) => {
+  let continueExec = true;
+  let lastRound = seats;
+  while (continueExec) {
+    const [changed, newSeats] = playRound2(lastRound);
+    lastRound = newSeats;
+    if (changed === 0) {
+      continueExec = false;
+    }
+  }
+  return lastRound;
+};
+
 const solveDay11Part1 = (data) => {
   const board = playUntilNoChange(data);
   const ss = SeatingSystem(board);
   const [countE, countO] = ss.countEmptyOccupied(board);
   return countO;
+};
+
+const solveDay11Part2 = (data) => {
+  const board = playUntilNoChange2(data);
+  const ss = SeatingSystem(board);
+  const [countE, countO] = ss.countEmptyOccupied(board);
+  return countO;;
 };
 
 /* 
@@ -107,6 +224,7 @@ import { readFileAndReturnArrayOfCharacterArrays } from './aoc-2020-api.mjs';
 const data =
   readFileAndReturnArrayOfCharacterArrays('./aoc-2020-day11.txt');
 console.log('Occupied seats after playing', solveDay11Part1(data));
+console.log('Occupied seats after playing', solveDay11Part2(data));
  */
 
 /**
@@ -115,8 +233,10 @@ console.log('Occupied seats after playing', solveDay11Part1(data));
  */
 
 console.log('Occupied seats after playing', solveDay11Part1(testData));
+console.log('Occupied seats after playing', solveDay11Part2(testData));
 
 /**
  * $ node ./aoc-2020-day11.js
  * Occupied seats after playing 37
+ * Occupied seats after playing 26
  */
