85b4cc5a0a10d78f291bcf76774a2169926a2bfa
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-12-12 02:15:56 +0100

    Cleane for day 10, part1

--- aoc-2020/aoc-2020-day10.js
+++ aoc-2020/aoc-2020-day10.js
@@ -1,25 +1,35 @@
-const testData1 = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4,];
+const testData1 = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
 const testData2 = [28, 33, 18, 42, 31, 14, 46, 20,
   48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25,
-  35, 8, 17, 7, 9, 4, 2, 34, 10, 3]
+  35, 8, 17, 7, 9, 4, 2, 34, 10, 3];
 
 const countValEqualsInArray = (val, arr) => {
   return arr.reduce((acc, curr) => (
     curr === val ? acc + 1 : acc), 0);
 };
 
-const solveDay10Part1 = (data) => {
+const generateJoltages = (data) => {
   const joltages = [...data].sort((a, b) => {
     return a - b;
   });
+  joltages.unshift(0);
   joltages.push(joltages.slice(-1)[0] + 3);
-  const diffs = joltages.map((joltage, i, arr) => {
+  return joltages;
+}
+
+const solveDay10Part1 = (data) => {
+  const joltages = generateJoltages(data);
+  const diffs = joltages.slice(1).map((joltage, i, arr) => {
     if (i === 0) { return joltage; }
     else { return joltage - arr[i - 1]; }
   });
   const diff1s = countValEqualsInArray(1, diffs);
   const diff3s = countValEqualsInArray(3, diffs);
   return diff1s * diff3s;
+};
+
+const solveDay10Part2 = (data) => {
+  return data;
 };
 
 /* 
@@ -36,6 +46,7 @@ console.log(solveDay10Part1(data));
 
 console.log(solveDay10Part1(testData1));
 console.log(solveDay10Part1(testData2));
+console.log(solveDay10Part2(testData1));
 
 /**
  * $ node aoc-2020-day10.js
